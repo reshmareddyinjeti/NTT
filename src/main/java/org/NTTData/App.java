aed1471c92fea22b066157477bd799d12565552c
reshmareddyinjeti <47693116+reshmareddyinjeti@users.noreply.github.com>
2022-09-01 19:11:58 +0100

    this is my first commit for ntt data

---
+++ src/main/java/org/NTTData/App.java
@@ -0,0 +1,13 @@
+package org.NTTData;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/NTTData/AppTest.java
@@ -0,0 +1,25 @@
+package org.NTTData;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+
+        System.out.println("Ntt data ");
+        System.out.println( "landg");
+        System.out.println("15");
+
+        assertTrue( true );
+    }
+}
